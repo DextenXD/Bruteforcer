03c77de5e052dab4979d464b47e265bb3879332a
DextenXD <joshklamer4@gmail.com>
2026-02-17 17:42:48 +0100

    Fixed attempts and added a timer

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-password = "password"
+import time
+
+password = "123456"
+start_time = time.perf_counter()
 
 with open('rockyou.txt', 'rt', encoding='latin-1') as f:
   attempt = 0
   for line in f:
+    attempt += 1
     if password == line.strip():
-      attempt =+ 1
-      print(f"Password is {line.strip()}. It took {attempt} try")
+      end_time = time.perf_counter()
+      elapsed = end_time - start_time
+      print(f"Password is {line.strip()}. It took {attempt} try, Time eslapsed: {elapsed:.6f} seconds.")
+    
       break
   else:
-    print("Password not found. Try a different wordlist")
+    end_time = time.perf_counter()
+    elapsed = end_time - start_time
+    print(f"Password not found. Try a different wordlist Time eslapsed: {elapsed:.6f} seconds.")
